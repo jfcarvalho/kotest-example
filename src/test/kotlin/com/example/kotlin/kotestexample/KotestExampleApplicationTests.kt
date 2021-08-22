710e1644b05e4b9ee58a8fa1a9eb30401a0145f4
jfcarvalho <jfcarvalho.profissional@gmail.com>
2021-08-22 10:28:49 -0300

    Subida dos arquivos iniciais

---
+++ src/test/kotlin/com/example/kotlin/kotestexample/KotestExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.kotestexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotestExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
